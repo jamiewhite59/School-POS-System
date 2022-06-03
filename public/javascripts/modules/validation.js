45ef5cf176f989f4640c46a49da44f944c2b0b83
jamiewhite59 <jamiewhite59@outlook.com>
2022-06-03 11:32:57 +0100

    Comment

--- public/javascripts/modules/validation.js
+++ public/javascripts/modules/validation.js
@@ -10,6 +10,7 @@ let validateEmail = (input) => !!input.match(/[a-z]+@school\.com/g);
 // Return true if match is found.
 let validatePostcode = (input) => !!input.match(/([A-Z]|[a-z]){2}(([A-Z]|[a-z])|[1-9]{1,2})\s?[1-9]([A-Z]|[a-z]){2}/g);
 
-try{
+try{ 
+  // To allow the test file to access the functions without showing an error on browser terminal.
   module.exports = {validateInput, validateEmail, validatePostcode};
 } catch (err){}
